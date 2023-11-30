dea689ece55b14ae7b52ff531a13a78a8617699b
Rodrigo Vinicius Leandrin <108472050+RodrigoLeandrin@users.noreply.github.com>
2023-11-29 23:00:33 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+alert ("this works!");
+
+
+var number = 0;
+var group = []
+
+while (number <= 1000) {
+    number++;
+    group.push(number);
+
+}
+alert (group);
+$(".container").text(group);
+
+
